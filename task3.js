792063c2271fa1029da9ccdf4d17eb5a4363fbce
John201858 <kryloff.andrey2015@yandex.ru>
2018-06-06 10:05:37 +0300

    Поменял кодировку в task3

--- task3.js
+++ task3.js
@@ -2,40 +2,40 @@ var numberOne = parseInt(process.argv[2]);
 var numberTwo = parseInt(process.argv[3]);
 
 function clockMinutesSeconds(numberOne,numberTwo) {
-//----------Находим сумму чисел-------------------------------------------------
+//----------РќР°С…РѕРґРёРј СЃСѓРјРјСѓ С‡РёСЃРµР»-------------------------------------------------
     var amountNumber = numberOne + numberTwo;
 //------------------------------------------------------------------------------
 
-//----------Находим часы--------------------------------------------------------
+//----------РќР°С…РѕРґРёРј С‡Р°СЃС‹--------------------------------------------------------
     clock = Math.floor(amountNumber / 3600);
 //------------------------------------------------------------------------------
 
-//----------Находим минуты------------------------------------------------------
+//----------РќР°С…РѕРґРёРј РјРёРЅСѓС‚С‹------------------------------------------------------
     minutes = Math.floor(amountNumber % 3600 / 60);
 //------------------------------------------------------------------------------
 
-//----------Находим секунды-----------------------------------------------------
+//----------РќР°С…РѕРґРёРј СЃРµРєСѓРЅРґС‹-----------------------------------------------------
     seconds = Math.floor(amountNumber % 60);
 //------------------------------------------------------------------------------ 
     clockName = '';
     minutesName = '';
     secondsName = '';
     
-    if ((clock % 10) == 1) { clockName = ' час ';}
-    if (((clock % 10) >= 2) && ((clock % 10) < 5)) { clockName = ' часа ';}
-    if (((clock % 10) == 0) || ((clock % 10) >= 5)) { clockName = ' часов ';}
+    if ((clock % 10) == 1) { clockName = ' С‡Р°СЃ ';}
+    if (((clock % 10) >= 2) && ((clock % 10) < 5)) { clockName = ' С‡Р°СЃР° ';}
+    if (((clock % 10) == 0) || ((clock % 10) >= 5)) { clockName = ' С‡Р°СЃРѕРІ ';}
     
-    if ((minutes % 10) == 1) { minutesName = ' минута ';}
-    if (((minutes % 10) >= 2) && ((minutes % 10) < 5)) { minutesName = ' минуты ';}
-    if (((minutes % 10) == 0) || ((minutes % 10) >= 5)) { minutesName = ' минут ';}
+    if ((minutes % 10) == 1) { minutesName = ' РјРёРЅСѓС‚Р° ';}
+    if (((minutes % 10) >= 2) && ((minutes % 10) < 5)) { minutesName = ' РјРёРЅСѓС‚С‹ ';}
+    if (((minutes % 10) == 0) || ((minutes % 10) >= 5)) { minutesName = ' РјРёРЅСѓС‚ ';}
     
-    if ((seconds % 10) == 1) { secondsName = ' секунда';}
-    if (((seconds % 10) >= 2) && ((seconds % 10) < 5)) { secondsName = ' секунды';}
-    if (((seconds % 10) == 0) || ((seconds % 10) >= 5)) { secondsName = ' секунд';} 
+    if ((seconds % 10) == 1) { secondsName = ' СЃРµРєСѓРЅРґР°';}
+    if (((seconds % 10) >= 2) && ((seconds % 10) < 5)) { secondsName = ' СЃРµРєСѓРЅРґС‹';}
+    if (((seconds % 10) == 0) || ((seconds % 10) >= 5)) { secondsName = ' СЃРµРєСѓРЅРґ';} 
     
-    if (Math.floor(clock / 10) == 1) {clockName = ' часов ';}
-    if (Math.floor(minutes / 10) == 1) {minutesName = ' минут ';}
-    if (Math.floor(seconds / 10) == 1) {secondsName = ' секунд';}
+    if (Math.floor(clock / 10) == 1) {clockName = ' С‡Р°СЃРѕРІ ';}
+    if (Math.floor(minutes / 10) == 1) {minutesName = ' РјРёРЅСѓС‚ ';}
+    if (Math.floor(seconds / 10) == 1) {secondsName = ' СЃРµРєСѓРЅРґ';}
     
     var result = clock + clockName + minutes + minutesName + seconds + secondsName;
 
